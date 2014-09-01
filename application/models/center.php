8aa358ca868be53d2ca1570da52834707e263407
nkimadusanka <nkimadusankaucsc@gmail.com>
2014-09-01 18:26:53 +0530

    add untrack models file to repo

---
+++ application/models/center.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Center extends CI_model{
+	var cId;
+	var province;
+	var address1;
+	var address2;
+	var phone;
+	var lLongitude;
+	var lLatitude;
+	var type;
+}
---
+++ application/models/mobile_session.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Donation_method extends CI_model{
+	var mseId;
+	var orgName;
+	var address1;
+	var address2;
+	var phone;
+	var date;
+	var stime;
+	var etime;
+	var lLongitude;
+	var lLatitude;
+	var eParticipation;
+}
