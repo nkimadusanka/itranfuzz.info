7c615a1679db1c4563869dd2d2143c621ef5769c
nkimadusanka <nkimadusankaucsc@gmail.com>
2014-09-10 00:07:14 +0530

    make map script to select location

--- js/map.js
+++ js/map.js
@@ -2,11 +2,23 @@ $(document).ready(function(){
 
 /*initializing map div*/
 function initialize() {
-            var mapOptions = {
-              center: new google.maps.LatLng(6.971445,79.922209),
-              zoom: 15
-            };
-            var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+	var marker = new google.maps.Marker(null);
+    var mapOptions = {
+        center: new google.maps.LatLng(6.971445,79.922209),
+            zoom: 15
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+    google.maps.event.addListener(map,'click',function(e){
+    	placeMarker(e.latLng, map);
+    });
+    function placeMarker(position, map){
+    	marker.setMap(null);
+    	marker = new google.maps.Marker({
+    		position: position,
+    		map: map
+    	});
+    	map.panTo(position);
+    }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
