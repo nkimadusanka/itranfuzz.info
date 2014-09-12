8336a1dae146d4ac141b108e78cccd0e15ecef5f
nkimadusanka <nkimadusankaucsc@gmail.com>
2014-09-12 21:48:54 +0530

    change bad var definitions in models

--- application/models/event.php
+++ application/models/event.php
@@ -1,9 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Event extends CI_model{
-	var evId;
-	var description;
-	var evType;
-	var center_cId;
-	var employee_eId;
+	var $evId;
+	var $description;
+	var $evType;
+	var $center_cId;
+	var $employee_eId;
 }
