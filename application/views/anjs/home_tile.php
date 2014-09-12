425494c5166fcaff5dea96ea1ea52189a55c0ef9
nkimadusanka <nkimadusankaucsc@gmail.com>
2014-09-12 22:13:56 +0530

    add dummy data to home page to look different

--- application/views/anjs/home_tile.php
+++ application/views/anjs/home_tile.php
@@ -1,2 +1,27 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
-<h1>Hello world fdldkfjdlkfjdlfkjdlfkjdlfkjdlkj</h1>
+<h1>Hello this is home tile</h1>
+<div>
+	<button>Test</button>
+</div>
+<section class="container-fluid" id="section4">
+		<h2 class="text-center">Register Now</h2>
+	    <div class="row">
+	      <div class="col-sm-8 col-sm-offset-2">
+	      </div>
+	    </div>
+</section>
+<section class="container-fluid" id="section3">
+		<h1 class="text-center">Bootstrap is Responsive</h1>
+	    <div class="row">
+	      <div class="col-sm-6 col-sm-offset-3">
+	        <p class="text-center lead">Vertical scrolling has become a popular and lasting trend in Web design. The question becomes, is it here to stay?</p>
+	        <div class="row">
+	          <div class="col-xs-6">Some brand-tacular designs even have home page content that is taller that 12,000 pixels. That's a lotta content.</div>
+	          <div class="col-xs-6 text-right">Anyhoo, this is just some random blurb of text, and Bootply.com is a playground and code editor for Bootstrap.</div>
+	        </div>
+	        <p class="text-center">
+	          <img src="assets/img_mtnpeople.png" class="img-responsive center-block ">
+	        </p>
+	      </div>
+	   </div>
+</section>
