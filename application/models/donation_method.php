c38a6c910e6284a5eb3083b39801168fe0a02afd
nkimadusanka <nkimadusankaucsc@gmail.com>
2014-09-12 21:49:53 +0530

    Another model error

--- application/models/donation_method.php
+++ application/models/donation_method.php
@@ -1,11 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Donation_method extends CI_model{
-	var dmId;
-	var description;
-	var minAge;
-	var maxAge;
-	var weight;
-	var dParticipating;
-	var maxAmount;
+	var $dmId;
+	var $description;
+	var $minAge;
+	var $maxAge;
+	var $weight;
+	var $dParticipating;
+	var $maxAmount;
 }
