e802b867b2ca906e2cde2f4098af735d5cfb3912
nkimadusanka <nkimadusankaucsc@gmail.com>
2014-09-18 15:18:19 +0530

    make footer boostrap validator disable on login

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -56,8 +56,11 @@
 		<script type="text/javascript" type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcRW4qdkVn49iIVJsjuATIdY4z05WJn1k"></script>
 		<!--Add all javascript lib in here-->
 		<script type="text/javascript" src="<?php echo base_url() ?>js/jquery/jquery.min.js"></script>
+		<!--Jquery validator-->
+		<script type="text/javascript" src="<?php echo base_url() ?>js/jquery/jquery.validate.min.js"></script>
+		<!--  boostrap js plugin -->
 		<script type="text/javascript" src="<?php echo base_url() ?>js/bootstrap/bootstrap.min.js"></script>
-		<script type="text/javascript" src="<?php echo base_url() ?>js/bootstrap/bootstrapValidator.min.js"></script>
+		<!-- add angular plugins -->
 		<script type="text/javascript" src="<?php echo base_url() ?>js/anjular/angular.js"></script>
 		<script type="text/javascript" src="<?php echo base_url() ?>js/anjular/angular-route.js"></script>
 		<script type="text/javascript" src="<?php echo base_url() ?>js/anjular/app.js"></script>
@@ -66,6 +69,7 @@
 		<!--smooth  scolling methods-->
 		<script type="text/javascript" src="<?php echo base_url() ?>js/smooth_scrolling.js"></script>
 		<script type="text/javascript" src="<?php echo base_url() ?>js/map.js"></script>
-<? }?>
+		<script type="text/javascript" src="<?php echo base_url() ?>js/bootstrap/bootstrapValidator.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url() ?>js/script.js"></script>
+<? }?>
 </html>
