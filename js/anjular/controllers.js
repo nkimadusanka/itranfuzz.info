2e569d948e57d70bcf52cc6ef1c462a163aacb40
Shihari Gunaratne <shihari.ucsc@gmail.com>
2014-10-17 12:02:16 -0700

    Addin donor table view controllers with center dummy data

--- js/anjular/controllers.js
+++ js/anjular/controllers.js
@@ -134,7 +134,10 @@ main_app.controller('staff_table', [
 			});
 		} ]);
 /* End of Staff Control methods */
-/* Start of donor manipulation methods */
+
+/* Start of donor manipulation controllers */
+
+//donor registration controller
 main_app.controller('donorControl',['$scope','$rootScope',
 	function($scope,$rootScope){
 		try {
@@ -144,4 +147,97 @@ main_app.controller('donorControl',['$scope','$rootScope',
 			} catch (e) {
 				window.alert("This is catch exception");
 			}
-	}]);
+}]);
+
+//donor table manipulation controller
+main_app.controller('donor_table', [
+		'$scope',
+		'$http',
+		'$rootScope',
+		function($scope, $http, $rootScope) {
+			$http.get("center_controller/getallcenters").success(
+					function(response) {
+						$scope.centers = response;
+					});
+
+			/* Remove scope fucntion */
+			$scope.removecenter = function(cId) {
+				for (var i = 0; i < $scope.centers.length; i++)
+					if ($scope.centers[i]["cId"] == cId)
+						break;
+				var mesg = $scope.centers[i]["address1"] + " "
+						+ $scope.centers[i]["address2"] + " "
+						+ $scope.centers[i]["province"]
+						+ " do you want to delete?";
+				if (window.confirm(mesg)) {
+					var data = $.param({
+						json : JSON.stringify({
+							cId : $scope.centers[i]["cId"]
+						})
+					});
+					$http.post('center_controller/removecenter', {
+						cId : $scope.centers[i]["cId"]
+					}).success(function(respone) {
+						if (respone.STATUS = 1) {
+							window.alert("add successfuly");
+						}
+					});
+				}
+			};
+			/* View scope function */
+			$scope.viewcenter = function(cId) {
+				for (var i = 0; i < $scope.centers.length; i++)
+					if ($scope.centers[i]["cId"] == cId)
+						break;
+				$scope.province = $scope.centers[i]["province"];
+				$scope.address1 = $scope.centers[i]["address1"];
+				$scope.address2 = $scope.centers[i]["address2"];
+				$scope.phone = $scope.centers[i]["phone"];
+				$scope.type = $scope.centers[i]["type"];
+
+				try {
+					$rootScope.initialize();
+					$rootScope.marker.setMap(null);
+					$rootScope.marker = new google.maps.Marker({
+						position : {
+							lat : parseFloat($scope.centers[i]["lLatitude"]),
+							lng : parseFloat($scope.centers[i]["lLongitude"])
+						},
+						map : $rootScope.map
+					});
+				} catch (e) {
+					sMessage("Error", "Error in google map");
+				}
+				$('.clustview-msg-model').modal('toggle');
+			}
+			$scope.updatecenter = function(cId) {
+
+				$('.clustupdate-msg-model').modal('toggle');
+
+				for (var i = 0; i < $scope.centers.length; i++)
+					if ($scope.centers[i]["cId"] == cId)
+						break;
+
+				$scope.province = $scope.centers[i]["province"];
+				$scope.address1 = $scope.centers[i]["address1"];
+				$scope.address2 = $scope.centers[i]["address2"];
+				$scope.phone = $scope.centers[i]["phone"];
+				$scope.type = $scope.centers[i]["type"];
+
+				try {
+					$rootScope.initialize(1);
+					clusterFormValid();
+					$rootScope.marker.setMap(null);
+					$rootScope.marker = new google.maps.Marker({
+						position : {
+							lat : parseFloat($scope.centers[i]["lLatitude"]),
+							lng : parseFloat($scope.centers[i]["lLongitude"])
+						},
+						map : $rootScope.map
+					});
+				} catch (e) {
+					sMessage("Error", "Error in google map");
+				}
+			}
+		} ]);
+//end of donor manipulation controllers
