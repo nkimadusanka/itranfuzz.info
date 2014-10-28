169f7dfcbf74fb23303b5f184e3476c0a7fd272a
nkimadusanka <nkimadusankaucsc@gmail.com>
2014-10-28 06:09:18 +0530

    Update donor get all error

--- application/models/donor.php
+++ application/models/donor.php
@@ -215,10 +215,19 @@ class Donor extends CI_model {
 	public function getAllDonors(){
 		$rows = $this->db->get('donor');
 		if($rows->num_rows() > 0){
-			return new Donor($rows->first_row());
+			$donors = array();
+			foreach($rows->first_row() as $r) {
+				array_push($donors,new Donor($rows->first_row()));
+			}
+			return $donors;
 		}else{
 			return null;
 		}	
+	}
+	//remove donor by id
+	public function removedonorbyid($dId){
+		$this->db->where('dId', $dId);
+		return($this->db->delete('donor'));
 	}
 	
 	// **************************************** getter function here******************************************************
