fb19c744553d7e49295c121a0bafb762d87eadc5
nkimadusanka <nkimadusankaucsc@gmail.com>
2014-10-28 06:09:54 +0530

    update home tile

--- application/views/anjs/home_tile.php
+++ application/views/anjs/home_tile.php
@@ -1,1 +1,171 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<div class="row" style="padding: 10px;" ng-controller="mainController">
+	<div class="col-md-6">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h3 class="panel-title">Today Events</h3>
+			</div>
+			<div class="panel-body">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8 col-sm-offset-1">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="panel panel-warning">
+			<div class="panel-heading">
+				<h3 class="panel-title">Upcomming Events</h3>
+			</div>
+			<div class="panel-body">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8 col-sm-offset-1">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8 col-sm-offset-1">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8 col-sm-offset-1">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Today Events</h3>
+			</div>
+			<div class="panel-body" style="padding: 0px;">
+				<div id="map-canvas" style="height: 600px; width: 100%;"/>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
+<div class="row" style="padding: 10px;">
+	<div class="panel panel-success">
+			<div class="panel-heading">
+				<h3 class="panel-title">Past Events</h3>
+			</div>
+			<div class="panel-body">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url()?>icons/event.png"
+									style="height: 60px; width: 60px;">
+							</div>
+							<div class="col-sm-8">
+								<h5>Date 2014/10/24 Time 8:00 AM</h5>
+								<h6 style="text-align: justify;">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+									scelerisque ante sollicitudin commodo. Cras purus odio,
+									vestibulum in.<a>Read more</a></h6>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
